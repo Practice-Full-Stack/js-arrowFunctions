c604e0195049f44ac00edf993c0ddb14d6275e15
James <jamesnorbertowork@gmail.com>
2021-01-08 08:43:07 +0900

    save

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-/******************************
+/***********************************
 STANDARD VS ARROW FUNCTION
-******************************/
+***********************************/
 
 // add two numbers
 let add = function(x,y) {
@@ -14,35 +14,51 @@ let add2 = (x,y) => (x + y);
 console.log(add(5,10));
 console.log(add2(10,10));
 
-/******************************
-"COMPARE FUNCTION"
-******************************/
+/***********************************
+ABOUT COMPARE FUNCTIONS
+***********************************/
 
-// for ascending use the .sort() method
+// for ascending use the sort() method
 let strings = ["cake", "apple", "fig", "banana"];
 console.log(strings.sort());
 
-// for descending combine with .reverse()
+// for descending combine sort() with reverse()
 let strings2 = ["cake", "apple", "banana", "fig"];
 strings2.sort();
 console.log(strings.reverse());
+console.log("-----")
 
-// NUMBERS ARE HARDER
+// for ascending numbers use sort()
 let nums = [50, 10, 90, 20];
-nums.sort(function(a,b){
+console.log(nums.sort());
+
+/********** USE COMPARE FUNCTION **********/
+
+// for descending numbers  we need to create a "compare function" that returns -1, 0, or 1
+// if compareFunction(a,b) returns less than 0, sort "a" lower than "b"
+// if compareFunction(a,b) returns 0, leave "a" and "b" unchanged ...
+// if compareFunction(a,b) returns greater than 0, sort "b" lower than "a"
+
+function compareFunction(a,b) {return b-a;}
+
+// use function logic to sort "nums"
+nums.sort(compareFunction);
+console.log(nums);
+console.log("-----")
+
+/********** NEST COMPARE FUNCTION **********/
+
+// nesting a local compare function is also possible
+nums2 = [200, 100, 600, 300];
+nums2.sort(function(a,b){
     return b-a;
 })
+console.log(nums2);
 
-// console.log(nums.sort());
+// revert to ascending via "compare function"
+console.log(nums2.sort(function(a,b){
+    return a-b;
+}));
 
-// USE A "COMPARE FUNCTION"
-nums.push(30);
-console.log(nums);
+/********** COMPARE ARROW FUNCTION **********/
 
-
-
-
-
-// arrow function
-// let numbers2 = [10, 30, 20];
-// numbers2.sort((a,b) => b - a)
